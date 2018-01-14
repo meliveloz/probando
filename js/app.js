3665684186d38e1bb456af20ac7306d534deacb3
meliveloz <mvelozcampos@gmail.com>
2018-01-14 01:41:16 -0300

    check

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@
   var token = result.credential.accessToken;
   // The signed-in user info.
   var user = result.user;
-  console.log(user);
+  console.log(user.displayName);
   // ...
 }).catch(function(error) {
   // Handle Errors here.
