ddc785ae8f8237769231abf999d2240eb66329ed
meliveloz <mvelozcampos@gmail.com>
2018-01-14 01:50:07 -0300

    change

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@
   // The signed-in user info.
   var user = result.user;
   console.log(user.displayName);
-  $("p").html(displayName);
+  $("p").html(user.displayName);
   // ...
 }).catch(function(error) {
   // Handle Errors here.
